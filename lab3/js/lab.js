ac83780af35086d75f27111520101425bf733533
solowohashi <solowohashi@gmai.com>
2021-01-23 15:58:45 -0800

    new

---
+++ lab3/js/lab.js
@@ -0,0 +1,12 @@
+// Print 8x8 board
+// Print pieces
+// Set rules for how pieces move
+// Check for legal moves
+// Check for checkmate/stalemate
+//  If checkmate/stalemate, game ends
+// Set boolean for turns
+//  True: White (White moves first)
+//  False: Black
+// Accept user input by dragging a piece to a different square.
+// After White moves, set boolean to False
+// Black moves
