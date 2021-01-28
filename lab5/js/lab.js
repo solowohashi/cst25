4cc52726552ca6f3bfeb08edf1382a8b1c36c512
solowohashi <solowohashi@gmai.com>
2021-01-27 18:34:04 -0800

    new

---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+/**
+* Author: Elijah Solow-Ohashi
+* Created: 1.27.2021
+* License: Public Domain
+*/
+
+//Define Variables
+var year = 1995;
+var model = "ascent ex";
+var make = "diamond back";
+var color = "color";
+var age = 2021 - year;
+var ownIt = true;
+console.log(make);
+console.log("Age: " + year);
+
+//Write Element
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Age: " + age + "<br>");
