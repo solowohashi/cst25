055f9a82dbcae28bfb68ad8dcb161a38cdbb0f50
solowohashi <solowohashi@gmai.com>
2021-02-11 08:14:13 -0800

    Update lab.js

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -2,18 +2,31 @@
 //date: 10 February 2021
 //license: MIT
 
+//console log test
 console.log("fucking christ");
 
+//referring to div in HTML
 var outputEl =
   document.getElementById("lab9content");
 
+//creates element
 var new1El =
   document.createElement("p");
   new1El.innerHTML = "something new";
 
+//creates element
 var new2El =
   document.createElement("p");
   new2El.innerHTML = "something else";
 
+//creates element
+var new3El =
+  document.createElement("p");
+  new3El.innerHTML = "something good";
+
+//links elements together
 outputEl.appendChild(new1El);
 outputEl.appendChild(new2El);
+
+//prepends element
+outputEl.prepend(new3El);
